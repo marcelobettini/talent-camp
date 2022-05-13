e24ef39bbfcc7eb586fd0eccf672af70bef04740
unknown <marcelobettini@yahoo.com.ar>
2022-05-13 15:58:49 -0300

    Node Express Handlebars

--- node01/app.js
+++ node01/app.js
@@ -1,16 +1,28 @@
 const express = require("express");
+const hbs = require("express-handlebars")
+const data = require("./data.json")
 const path = require("path")
 const PORT = process.env.PORT || 3000
 const app = express()
 app.use(express.static("public"))
 
+//Express-Handelbars settings
+app.engine(".hbs", hbs.engine({ extname: "hbs" })) //la extensión por default es .handlebars
+app.set("view engine", "hbs")
+app.set("views", "./views")
+
 //esto se ignora mientras haya un archivo index.html en la carpeta static ("public")
 app.get("/", (req, res) => {
-    res.send("Ruta raíz - Home Page")
+    res.render("home")
 })
 
 app.get("/users", (req, res) => {
-    res.send("no me importan tus query string")
+    res.render("users", { users: data })
+})
+app.get("/users/:id", (req, res, next) => {
+    const { id } = req.params
+    const user = data.find((usr) => usr.id === +id);
+    user ? res.status(200).send(user) : next()
 })
 //las rutas  pueden recibir Query params (query strings) sin hacerles ningún tipo de preparación adicional. El formato para realizar una consulta de este modo es: ruta?clave=valor por ejemplo http://mitienda.com/productos?plancha=atma. Las query strings viajarán al backend en el objeto query de la petición o request. Para pasar más de un query param, deben concatenar con &, sin repetir el ?
 app.get("/products", (req, res) => {
@@ -33,9 +45,18 @@ app.get("/products/:id/:name", (req, res) => {
 
 
 //catch all route (404)
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notFound.html")); //send normal HTML file
+app.get("*", (req, res, next) => {
+    let error = new Error("Recurso no encontrado");
+    error.status = 404;
+    next(error)
 });
+
+//error handler
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    console.log(error)
+    res.render("error", { error, layout: "clean" })
+})
 
 
 
